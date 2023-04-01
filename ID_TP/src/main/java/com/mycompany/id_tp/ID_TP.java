5ad6478aefda5009d1626a22bbc9d3f1d0876d49
Luís Travassos <HenriqueTravassos03@outlook.com>
2023-04-01 19:48:49 +0100

    projeto iniciado

---
+++ ID_TP/src/main/java/com/mycompany/id_tp/ID_TP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.id_tp;
+
+/**
+ *
+ * @author luist
+ */
+public class ID_TP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
